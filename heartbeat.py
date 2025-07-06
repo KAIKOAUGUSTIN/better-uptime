afde6888d6b8e960fdc7a246c3bc483138c3912d
KAIKOAUGUSTIN <114446391+KAIKOAUGUSTIN@users.noreply.github.com>
2025-07-06 20:51:28 -0300

    Update heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -7,4 +7,5 @@ while True:
         print(f"Heartbeat enviado. Código de status: {response.status_code}")
     except requests.RequestException as e:
         print(f"Ocorreu um erro: {e}")
-    time.sleep(30)  # Aguarda 30 segundos antes de enviar novamente
+    time.sleep(20)
+    
