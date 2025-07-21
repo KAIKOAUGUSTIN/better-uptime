5bd76c8d0195c8537cf4634fedb8dc42ad0cb4f4
KAIKOAUGUSTIN <114446391+KAIKOAUGUSTIN@users.noreply.github.com>
2025-07-21 20:34:31 -0300

    Update heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -3,7 +3,7 @@ import requests
 
 while True:
     try:
-        response = requests.get("http://192.168.15.11:3001/api/push/S3Rt80RcxCyJoh3dyl9zrPjQARwGurBj?status=up&msg=OK&ping=")
+        response = requests.get("http://192.168.15.11:3001/api/push/UIqJL2m3Qt3aMLnziYV5AO1p6E0NViCt?status=up&msg=OK&ping=")
         print(f"Heartbeat enviado. Código de status: {response.status_code}")
     except requests.RequestException as e:
         print(f"Ocorreu um erro: {e}")
